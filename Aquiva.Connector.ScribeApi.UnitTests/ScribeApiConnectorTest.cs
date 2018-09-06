3b575c700251fb4390efd66d6c0075b7a56f672a
Vladimir Almaev <almaev.vladimir@gmail.com>
2018-09-06 15:31:57 +0300

    Add ScribeApiConnector class annotated with ScribeConnectorAttribute

--- Aquiva.Connector.ScribeApi.UnitTests/ScribeApiConnectorTest.cs
+++ Aquiva.Connector.ScribeApi.UnitTests/ScribeApiConnectorTest.cs
@@ -1,6 +1,93 @@
-﻿namespace Aquiva.Connector.ScribeApi
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using Scribe.Core.ConnectorApi;
+using Xunit;
+
+namespace Aquiva.Connector.ScribeApi
 {
     public class ScribeApiConnectorTest
     {
+        [Fact]
+        public void ScribeApiConnector_Always_ShouldBeDeclaredAsPublic()
+        {
+            var actual = typeof(ScribeApiConnector);
+            Assert.True(actual.IsPublic);
+        }
+
+        [Fact]
+        public void ScribeApiConnector_Always_ShouldProvideDefaultConstructorThatDoesNotThrow()
+        {
+            var actual = typeof(ScribeApiConnector).GetConstructor(Type.EmptyTypes);
+            Assert.NotNull(actual);
+            actual.Invoke(null);
+        }
+
+        [Fact]
+        public void ScribeApiConnector_Always_ShouldBeTheOnlyTypeMarkedWithScribeConnectorAttribute()
+        {
+            var actual = typeof(ScribeApiConnector)
+                .Assembly
+                .GetTypes()
+                .Where(x => Attribute.IsDefined(x, typeof(ScribeConnectorAttribute)));
+            Assert.Single(actual);
+        }
+
+        [Fact]
+        public void ScribeApiConnector_Always_ShouldHaveProperSupportedSolutionRoles()
+        {
+            var actual = typeof(ScribeApiConnector)
+                .GetCustomAttributes<ScribeConnectorAttribute>()
+                .Single()
+                .SupportedSolutionRoles;
+
+            Assert.Contains("Scribe.IS2.Source", actual);
+            Assert.Contains("Scribe.IS2.Target", actual);
+        }
+
+        [Fact]
+        public void ScribeApiConnector_Always_ShouldHaveProperConnectorType()
+        {
+            var actual = typeof(ScribeApiConnector)
+                .GetCustomAttributes<ScribeConnectorAttribute>()
+                .Single()
+                .ConnectorType;
+
+            Assert.Equal(typeof(ScribeApiConnector), actual);
+        }
+
+        [Fact]
+        public void ScribeApiConnector_Always_ShouldSupportCloudAgents()
+        {
+            var actual = typeof(ScribeApiConnector)
+                .GetCustomAttributes<ScribeConnectorAttribute>()
+                .Single()
+                .SupportsCloud;
+
+            Assert.True(actual);
+        }
+
+        [Fact]
+        public void ScribeApiConnector_Always_ShouldUseGenericConnectionUI()
+        {
+            var actual = typeof(ScribeApiConnector)
+                .GetCustomAttributes<ScribeConnectorAttribute>()
+                .Single();
+
+            Assert.Equal("ScribeOnline.GenericConnectionUI", actual.ConnectionUITypeName);
+            Assert.Equal("1.0.0.0", actual.ConnectionUIVersion);
+        }
+
+        [Fact]
+        public void ScribeApiConnector_Always_ShouldNotUseSettingsUI()
+        {
+            var actual = typeof(ScribeApiConnector)
+                .GetCustomAttributes<ScribeConnectorAttribute>()
+                .Single();
+
+            Assert.Empty(actual.SettingsUITypeName);
+            Assert.Equal("1.0.0.0", actual.SettingsUIVersion);
+            Assert.Empty(actual.XapFileName);
+        }
     }
 }
--- Aquiva.Connector.ScribeApi/ScribeApiConnector.cs
+++ Aquiva.Connector.ScribeApi/ScribeApiConnector.cs
@@ -1,6 +1,21 @@
-﻿namespace Aquiva.Connector.ScribeApi
+﻿using Scribe.Core.ConnectorApi;
+
+namespace Aquiva.Connector.ScribeApi
 {
-    public class ScribeApiConnector
+    [ScribeConnector(
+        name: "TIBCO Scribe Platform API (Demo)",
+        description: "Metaconnector which connects platform to itself.",
+        connectorTypeId: "e0e8461e-92ae-4b5f-980b-a2dd104b7d24",
+        connectorType: typeof(ScribeApiConnector),
+        connectorVersion: "0.0.0.0",
+        supportedSolutionRoles: new[] {"Scribe.IS2.Source", "Scribe.IS2.Target"},
+        supportsCloud: true,
+        connectionUITypeName: "ScribeOnline.GenericConnectionUI",
+        connectionUIVersion: "1.0.0.0",
+        settingsUITypeName: "",
+        settingsUIVersion: "1.0.0.0",
+        xapFileName: "")]
+    public sealed class ScribeApiConnector
     {
     }
 }
